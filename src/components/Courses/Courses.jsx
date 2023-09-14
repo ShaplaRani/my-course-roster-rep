3dc6d7ee36233f86071cdbe966f5af90a75352ce
ShaplaRani <shaplarani621@gmail.com>
2023-09-14 10:50:12 -0700

    The courses section has been made responsive for small and medium devices

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -13,7 +13,7 @@ const Courses = () => {
     return (
        <div className="md:w-3/4">
         
-        <div className=" grid grid-cols-3 gap-6">
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {
                 allCourses.map((course) => <Course key={course.id} course={course}></Course>)
             }
